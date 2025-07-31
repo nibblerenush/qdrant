b059a6b032ad6e5bd79b7bef263112bedcfea4c0
xzfc <xzfcpw@gmail.com>
2025-07-31 13:16:56 +0000

    Remove `impl VectorStorage for TestRawScorerProducer`

--- lib/segment/src/fixtures/index_fixtures.rs
+++ lib/segment/src/fixtures/index_fixtures.rs
@@ -1,19 +1,14 @@
 use std::borrow::Cow;
-use std::ops::Range;
-use std::path::PathBuf;
-use std::sync::atomic::AtomicBool;
 
-use bitvec::prelude::{BitSlice, BitVec};
+use bitvec::prelude::BitVec;
 use common::counter::hardware_counter::HardwareCounterCell;
 use common::types::PointOffsetType;
 use rand::Rng;
 
-use crate::common::Flusher;
-use crate::common::operation_error::OperationResult;
 use crate::data_types::named_vectors::CowVector;
 use crate::data_types::vectors::{DenseVector, VectorElementType, VectorRef};
 use crate::index::hnsw_index::point_scorer::FilteredScorer;
-use crate::types::{Distance, VectorStorageDatatype};
+use crate::types::Distance;
 use crate::vector_storage::dense::volatile_dense_vector_storage::new_volatile_dense_vector_storage;
 use crate::vector_storage::{VectorStorage, VectorStorageEnum};
 
@@ -25,77 +20,6 @@ pub struct TestRawScorerProducer {
     pub vector_storage: VectorStorageEnum,
     pub deleted_points: BitVec,
     pub distance: Distance,
-}
-
-impl VectorStorage for TestRawScorerProducer {
-    fn distance(&self) -> Distance {
-        self.vector_storage.distance()
-    }
-
-    fn datatype(&self) -> VectorStorageDatatype {
-        self.vector_storage.datatype()
-    }
-
-    fn is_on_disk(&self) -> bool {
-        self.vector_storage.is_on_disk()
-    }
-
-    fn total_vector_count(&self) -> usize {
-        self.vector_storage.total_vector_count()
-    }
-
-    fn get_vector(&self, key: PointOffsetType) -> CowVector {
-        self.vector_storage.get_vector(key)
-    }
-
-    fn get_vector_sequential(&self, key: PointOffsetType) -> CowVector {
-        self.vector_storage.get_vector_sequential(key)
-    }
-
-    fn get_vector_opt(&self, key: PointOffsetType) -> Option<CowVector> {
-        self.vector_storage.get_vector_opt(key)
-    }
-
-    fn insert_vector(
-        &mut self,
-        key: PointOffsetType,
-        vector: VectorRef,
-        hw_counter: &HardwareCounterCell,
-    ) -> OperationResult<()> {
-        self.vector_storage.insert_vector(key, vector, hw_counter)
-    }
-
-    fn update_from<'a>(
-        &mut self,
-        other_vectors: &'a mut impl Iterator<Item = (CowVector<'a>, bool)>,
-        stopped: &AtomicBool,
-    ) -> OperationResult<Range<PointOffsetType>> {
-        self.vector_storage.update_from(other_vectors, stopped)
-    }
-
-    fn flusher(&self) -> Flusher {
-        self.vector_storage.flusher()
-    }
-
-    fn files(&self) -> Vec<PathBuf> {
-        self.vector_storage.files()
-    }
-
-    fn delete_vector(&mut self, key: PointOffsetType) -> OperationResult<bool> {
-        self.vector_storage.delete_vector(key)
-    }
-
-    fn is_deleted_vector(&self, key: PointOffsetType) -> bool {
-        self.vector_storage.is_deleted_vector(key)
-    }
-
-    fn deleted_vector_count(&self) -> usize {
-        self.vector_storage.deleted_vector_count()
-    }
-
-    fn deleted_vector_bitslice(&self) -> &BitSlice {
-        self.vector_storage.deleted_vector_bitslice()
-    }
 }
 
 impl TestRawScorerProducer {
